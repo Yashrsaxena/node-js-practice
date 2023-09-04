b2928466e714eba743212220f368377d5bb39fe1
yasraj1 <yash.raj@publicissapient.com>
2023-09-04 16:46:44 +0530

    routing practice

--- 1-node-farm/starter/files/server.js
+++ 1-node-farm/starter/files/server.js
@@ -1,8 +1,24 @@
 const http = require('http');
+const url = require('url');
 
 // SERVER
 http.createServer((request, response) => {
-    response.end("Hello From the SERVER!");
+    pathName = request.url;
+    if(pathName === '/' || pathName === '/overview'){
+        response.end("This is an OVERVIEW!");
+    }
+    else if (pathName === '/product')
+    {
+        response.end('This is a PRODUCT!');
+    }
+    else
+    {
+        response.writeHead(404, {
+            'Content-type': 'text/html',
+            'my-own-header': 'hello-world'
+        });
+        response.end("<h1>PAGE NOT FOUND!</h1>");
+    }
 }).listen(8080, () => {
     console.log("Listening to server on port 8080");
 });
