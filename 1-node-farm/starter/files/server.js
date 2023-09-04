e519d6b9467105d219903e38d07fa0081c2c8b9d
yasraj1 <yash.raj@publicissapient.com>
2023-09-04 16:55:39 +0530

    dev-data fetched and routed

--- 1-node-farm/starter/files/server.js
+++ 1-node-farm/starter/files/server.js
@@ -1,7 +1,11 @@
+const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
 // SERVER
+/* We will read data synchronously in this case as we want to read data only once, hence doing it outside the createServer callback function*/
+const data = fs.readFileSync(`${__dirname}/../dev-data/data.json`, 'utf-8');
+// const dataObj = JSON.parse(data);
 http.createServer((request, response) => {
     pathName = request.url;
     if(pathName === '/' || pathName === '/overview'){
@@ -10,6 +14,11 @@ http.createServer((request, response) => {
     else if (pathName === '/product')
     {
         response.end('This is a PRODUCT!');
+    }
+    else if(pathName === '/api')
+    {
+        response.writeHead(200, {'Content-Type':'application/json'});
+        response.end(data);
     }
     else
     {
